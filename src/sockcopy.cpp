9080d6edf219ceb684f815ed7c699d1095eb16c9
Daniel M <daniel.q.mueller@stud.h-da.de>
2021-05-29 15:42:45 +0200

    Fix for real

--- src/sockcopy.cpp
+++ src/sockcopy.cpp
@@ -4,38 +4,38 @@ namespace netlib
 {
 
 
-SockCopy<TcpStream> wrapCopy(TcpStream & other, bool disableAutoclose = true)
+SockCopy<TcpStream> wrapCopy(TcpStream & other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<TcpStream>(other);
 }
 
-SockCopy<TcpListener> wrapCopy(TcpListener & other, bool disableAutoclose = true)
+SockCopy<TcpListener> wrapCopy(TcpListener & other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<TcpListener>(other);
 }
 
-SockCopy<UdpSocket> wrapCopy(UdpSocket & other, bool disableAutoclose = true)
+SockCopy<UdpSocket> wrapCopy(UdpSocket & other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<UdpSocket>(other);
 }
 
 
-SockCopy<TcpStream> wrapCopy(TcpStream && other, bool disableAutoclose = true)
+SockCopy<TcpStream> wrapCopy(TcpStream && other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<TcpStream>(std::move(other));
 }
 
-SockCopy<TcpListener> wrapCopy(TcpListener && other, bool disableAutoclose = true)
+SockCopy<TcpListener> wrapCopy(TcpListener && other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<TcpListener>(std::move(other));
 }
 
-SockCopy<UdpSocket> wrapCopy(UdpSocket && other, bool disableAutoclose = true)
+SockCopy<UdpSocket> wrapCopy(UdpSocket && other, bool disableAutoclose)
 {
     if (disableAutoclose) other.setAutoclose(false);
     return SockCopy<UdpSocket>(std::move(other));
